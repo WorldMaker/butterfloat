b0d832ce9545e42ce47b84f545f2d70ccc7dc5fc
Max Battcher <me@worldmaker.net>
2023-10-26 13:51:30 -0400

    🔧 npm run prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,31 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'prettier',
-	],
-	overrides: [],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: ['@typescript-eslint'],
-	rules: {
-		'@typescript-eslint/no-unused-vars': [
-			'error',
-			{
-				argsIgnorePattern: '^_',
-				varsIgnorePattern: '^(_|jsx$)',
-				caughtErrorsIgnorePattern: '^_',
-			},
-		],
-		'no-case-declarations': 'warn',
-		'no-fallthrough': 'warn',
-	},
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^(_|jsx$)',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    'no-case-declarations': 'warn',
+    'no-fallthrough': 'warn',
+  },
 }
