91491b85c4815c3cc828c0a515402c49a81bfbc7
Max Battcher <me@worldmaker.net>
2023-11-14 15:45:09 -0500

    Core component wiring infrastructure

---
+++ wiring.ts
@@ -0,0 +1,122 @@
+import {
+  Observable,
+  Subscriber,
+  Subscription,
+  animationFrameScheduler,
+  delay,
+} from 'rxjs'
+import { ComponentContext, ComponentDescription } from './component.js'
+import { makeEventProxy } from './events.js'
+import { buildTree } from './static-dom.js'
+import { BindingContext, bindElement } from './binding.js'
+
+export interface WiringContext {
+  suspense?: Observable<boolean>
+  isStaticComponent: boolean
+  isStaticTree: boolean
+  preserveOnComplete?: boolean
+}
+
+export function wireInternal(
+  description: ComponentDescription,
+  subscriber: Subscriber<Node>,
+  context: WiringContext,
+) {
+  const subscription = new Subscription()
+
+  const error = (error: unknown) => {
+    console.error(`Error in component ${description.component.name}`, error)
+  }
+  const complete = () => subscriber.complete()
+
+  const { events, handler } = makeEventProxy(description.component.name)
+
+  const componentContext: ComponentContext = {
+    bindEffect(observable, effect) {
+      context.isStaticComponent = false
+      subscription.add(
+        observable.pipe(delay(0, animationFrameScheduler)).subscribe({
+          next: effect,
+          error,
+          complete,
+        }),
+      )
+    },
+    bindImmediateEffect(observable, effect) {
+      context.isStaticComponent = false
+      subscription.add(
+        observable.subscribe({
+          next: effect,
+          error,
+          complete,
+        }),
+      )
+    },
+    events,
+  }
+
+  const tree = description.component(description.properties, componentContext)
+  const { elementBinds, nodeBinds, container } = buildTree(tree)
+  context.isStaticComponent &&=
+    elementBinds.length === 0 && nodeBinds.length === 0
+  context.isStaticTree &&= context.isStaticComponent
+  subscriber.next(container)
+
+  const bindContext: BindingContext = {
+    complete,
+    error,
+    suspense: context.suspense,
+    eventBinder: handler,
+    subscription,
+  }
+
+  for (const [element, bindDescription] of elementBinds) {
+    bindElement(element, bindDescription, bindContext)
+  }
+
+  return () => {
+    subscription.unsubscribe()
+  }
+}
+
+export function wire(
+  description: ComponentDescription,
+  context: WiringContext,
+) {
+  return new Observable((subscriber: Subscriber<Node>) =>
+    wireInternal(description, subscriber, context),
+  )
+}
+
+export function run(
+  container: Node,
+  description: ComponentDescription,
+  subscription: Subscription,
+  context: WiringContext,
+  placeholder?: Node,
+) {
+  const observable = wire(description, context)
+  let previousNode: Node | null = null
+  subscription.add(
+    observable.subscribe({
+      next(node) {
+        if (previousNode) {
+          container.replaceChild(previousNode, node)
+        } else if (placeholder) {
+          container.replaceChild(placeholder, node)
+        } else {
+          container.appendChild(node)
+        }
+        previousNode = node
+      },
+      error(error) {
+        console.error(`Error in component ${description.component.name}`, error)
+      },
+      complete() {
+        if (!context.preserveOnComplete && previousNode) {
+          container.removeChild(previousNode)
+        }
+      },
+    }),
+  )
+}
