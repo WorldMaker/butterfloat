d426fc2f52980a6631d173dc7978651b6ffdc633
Max Battcher <me@worldmaker.net>
2023-11-26 18:01:44 -0500

    Document component children and dynamic children binding

---
+++ docs/children.test.tsx
@@ -0,0 +1,53 @@
+import { ok } from 'node:assert/strict'
+import { describe, it } from 'node:test'
+import { NEVER, concat, delayWhen, from, interval, map } from 'rxjs'
+import { Children, ComponentContext, jsx } from '../index'
+
+describe('children documentation', () => {
+  it('shows a simple list wrapper with children', () => {
+    interface CoolListProps {}
+
+    function CoolList(_props: CoolListProps, context: ComponentContext) {
+      return (
+        <div className="cool-list-wrapper">
+          <ul className="cool-list">
+            <Children context={context} />
+          </ul>
+        </div>
+      )
+    }
+
+    function ListPage() {
+      return (
+        <div className="list-page">
+          <CoolList>
+            <li>This</li>
+            <li>Is</li>
+            <li>Wrapped</li>
+            <li>In</li>
+            <li>A</li>
+            <li>Cool</li>
+            <li>List</li>
+          </CoolList>
+        </div>
+      )
+    }
+
+    ok(ListPage)
+  })
+
+  it('shows a dynamic list example', () => {
+    function DynamicList() {
+      const children = concat(
+        from(['This', 'is', 'a', 'dynamic', 'list']).pipe(
+          delayWhen((_v, i) => interval(i * 500)),
+        ),
+        NEVER,
+      ).pipe(map((text) => () => <li>{text}</li>))
+
+      return <li childrenBind={children} />
+    }
+
+    ok(DynamicList)
+  })
+})
